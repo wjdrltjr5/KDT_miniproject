cf37bac3f1857b7a1e3397e5ce60045d8f53783d
Jeong gi seok <wjdrltjr5@naver.com>
2024-04-12 11:25:40 +0900

    feat : id,pwd check #1

--- src/main/java/org/kdt/dto/MemberDTO.java
+++ src/main/java/org/kdt/dto/MemberDTO.java
@@ -1,4 +1,47 @@
 package org.kdt.dto;
 
 public class MemberDTO {
+    private String member_no;
+    private String member_name;
+    private String member_id;
+    private String member_passwd;
+    private String member_email;
+    private String member_balance;
+    private String member_role;
+
+    public String getMember_no() {
+        return member_no;
+    }
+
+    public String getMember_name() {
+        return member_name;
+    }
+
+    public String getMember_id() {
+        return member_id;
+    }
+
+    public String getMember_passwd() {
+        return member_passwd;
+    }
+
+    public String getMember_email() {
+        return member_email;
+    }
+
+    public String getMember_balance() {
+        return member_balance;
+    }
+
+    public String getMember_role() {
+        return member_role;
+    }
+
+    public void setMember_id(String member_id) {
+        this.member_id = member_id;
+    }
+
+    public void setMember_passwd(String member_passwd) {
+        this.member_passwd = member_passwd;
+    }
 }
--- src/main/java/org/kdt/service/MemberService.java
+++ src/main/java/org/kdt/service/MemberService.java
@@ -1,4 +1,30 @@
 package org.kdt.service;
 
+import org.apache.ibatis.session.SqlSession;
+import org.kdt.Config;
+import org.kdt.dao.MemberDAO;
+import org.kdt.dto.MemberDTO;
+
 public class MemberService {
+
+    private final MemberDAO dao;
+
+    public MemberService(MemberDAO dao) {
+        this.dao = dao;
+    }
+
+    public boolean login(MemberDTO memberDTO) {
+        try(SqlSession session = Config.getConnection()){
+            MemberDTO check = dao.findById(session, memberDTO.getMember_id());
+            return checkIdAndPassword(memberDTO,check);
+        }
+
+    }
+
+    private boolean checkIdAndPassword(MemberDTO memberDTO, MemberDTO check){
+        if(memberDTO.getMember_id().equals(check.getMember_id())
+                && memberDTO.getMember_passwd().equals(check.getMember_passwd())){
+            return true;
+        }else return false;
+    }
 }
