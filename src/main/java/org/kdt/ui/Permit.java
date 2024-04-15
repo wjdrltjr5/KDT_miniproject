5f75f01bc7c6546994d7960f221f1f703d98820c
Jeong gi seok <wjdrltjr5@naver.com>
2024-04-15 14:11:21 +0900

    feat : permit ui

--- src/main/java/org/kdt/ui/Permit.java
+++ src/main/java/org/kdt/ui/Permit.java
@@ -5,11 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Permit extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -37,6 +42,20 @@ public class Permit extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("\uC8FC\uBB38 \uBC88\uD638");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(12, 20, 100, 50);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(126, 20, 200, 50);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnPermit = new JButton("\uD5C8\uAC00\uD558\uAE30");
+		btnPermit.setBounds(126, 106, 200, 40);
+		contentPane.add(btnPermit);
 	}
-
 }
