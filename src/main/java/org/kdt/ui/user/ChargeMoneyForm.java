bb3cd38ad853277e57e032e13473b2fa237afdc8
신동구 <ggg9905@naver.com>
2024-04-17 10:18:20 +0900

    잔액에 , 추가

--- src/main/java/org/kdt/ui/user/ChargeMoneyForm.java
+++ src/main/java/org/kdt/ui/user/ChargeMoneyForm.java
@@ -13,66 +13,65 @@ import javax.swing.border.EmptyBorder;
 
 public class ChargeMoneyForm extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField textField;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField textField;
 
-	private MemberDTO memberDTO;
+    private MemberDTO memberDTO;
 
-	private final MemberService memberService;
+    private final MemberService memberService;
 
-	/**
-	 * Launch the application.
-	 */
+    public ChargeMoneyForm(MemberDTO memberDTO) {
+        this.memberDTO = memberDTO;
+        memberService = new MemberServiceImpl(new MemberDAO());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 350, 200);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-	/**
-	 * Create the frame.
-	 */
-	public ChargeMoneyForm(MemberDTO memberDTO) {
-		this.memberDTO = memberDTO;
-		memberService = new MemberServiceImpl(new MemberDAO());
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 350, 200);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        JLabel lblNewLabel = new JLabel("충전할 금액");
+        lblNewLabel.setBounds(12, 31, 77, 50);
+        contentPane.add(lblNewLabel);
+        
+        textField = new JTextField();
+        textField.setBounds(101, 32, 212, 50);
+        contentPane.add(textField);
+        textField.setColumns(10);
+        
+        JButton btnChargeMoney = new JButton("충전하기");
+        btnChargeMoney.setBounds(68, 105, 200, 40);
+        contentPane.add(btnChargeMoney);
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("충전할 금액");
-		lblNewLabel.setBounds(12, 31, 77, 50);
-		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(101, 32, 212, 50);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JButton btnChargeMoney = new JButton("충전하기");
-		btnChargeMoney.setBounds(68, 105, 200, 40);
-		contentPane.add(btnChargeMoney);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
 
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				dispose();
-			}
-		});
+        btnChargeMoney.addActionListener(e -> chargeMoneyBtnAction());
+    }
 
-		btnChargeMoney.addActionListener(e -> chargeMoneyBtnAction());
-	}
+    private void chargeMoneyBtnAction(){
+        int member_balance = memberDTO.getMember_balance();
+        int chargeAmount = Integer.parseInt(textField.getText());
+        int i = memberService.chargeMoney(memberDTO, chargeAmount);
+        if(i > 0){
+            JOptionPane.showMessageDialog(null, "충전에 성공하였습니다.\n현재 잔액: " + addCommasToNumber(member_balance + chargeAmount));
+            memberDTO.setMember_balance(member_balance + chargeAmount); // 충전된 금액을 현재 잔액에 추가
+            setVisible(false);
+        }else{
+            JOptionPane.showMessageDialog(null, "충전에 실패하였습니다.");
+            setVisible(false);
+        }
+    }
 
-	private void chargeMoneyBtnAction(){
-		int member_balance = memberDTO.getMember_balance();
-		int i = memberService.chargeMoney(memberDTO, Integer.parseInt(textField.getText()));
-		if(i > 0){
-			JOptionPane.showMessageDialog(null, "충전에 성공하였습니다.");
-			setVisible(false);
-		}else{
-			memberDTO.setMember_balance(member_balance);
-			JOptionPane.showMessageDialog(null, "충전에 실패하였습니다.");
-			setVisible(false);
-		}
-	}
+    // 숫자를 문자열로 변환하고 3자리마다 쉼표를 추가하는 메서드
+    private String addCommasToNumber(int number) {
+        return String.format("%,d", number);
+    }
 }
